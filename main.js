5a73b40671948de7134b301663d0bb1a36f532e0
Flovis <floviskabandafkm@gmail.com>
2023-02-28 16:56:33 +0200

    contact succes and error message added

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
 const navToggle = document.querySelector('.nav__toggle');
 const navMenu = document.querySelector('.nav__menu');
+const succesMessage = document.getElementById('succes');
+const dangerMessage = document.getElementById('danger');
+
 
 navToggle.addEventListener('click', () => {
   navMenu.classList.toggle('show-menu');
   navToggle.classList.toggle('show-icon');
 });
+
 /*===== SCROLL REVEAL ANIMATION =====*/
 const sr = ScrollReveal({
   origin: 'top',
@@ -31,6 +35,16 @@ form.addEventListener('submit', (e)=>{
   
 })
 
+const name = document.getElementById('name');
+const email = document.getElementById('name');
+const message = document.getElementById('name');
+// name.addEventListener('change', () => {
+//   if (name.value.length < 3) {
+//     let spanError = document.createElement('span');
+//     spanError.innerHTML = 'Le nom est trop court';
+//     name.insertAdjacentElement('afterend', spanError);
+//   }
+// })
 function send_email(){
   const SERVICE_ID = 'service_811k0va';
   const TEMPLATE_ID = 'template_g150p2o'
@@ -38,18 +52,26 @@ function send_email(){
     name: document.getElementById('name').value.trim(),
     email: document.getElementById('email').value.trim(),
     message: document.getElementById('message').value.trim(),
-
   }
   emailjs
   .send(SERVICE_ID, TEMPLATE_ID, params)
   .then((res) => {
-    alert('Message evoye');
+    // alert('Message evoye');
     btnSendMessage.innerHTML = btnText;
+
+    succesMessage.innerHTML = 'Your email has been received successfully';
+    setTimeout(function(){
+      succesMessage.innerHTML = '';
+    }, 3000)
     
   })
   .catch((err) => {
     console.log(err);
     btnSendMessage.innerHTML = btnText;
+    dangerMessage.innerHTML = 'Something went wrong';
+    setTimeout(function(){
+      dangerMessage.innerHTML = '';
+    }, 3000)
   });
 }
 
